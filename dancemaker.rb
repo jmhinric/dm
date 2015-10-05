caa72025d3738081daa57b98eaf39f6ccdf25402
John Hinrichs <jmhinric@gmail.com>
2015-10-05 11:40:30 -0500

    implement weighted move selection

--- dancemaker.rb
+++ dancemaker.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class DanceMaker
   NUM_TIMES = [
     "",
@@ -5,14 +7,14 @@ class DanceMaker
     "thrice"
   ]
 
-  attr_accessor :movements, :qualities, :duration, :scale
+  attr_accessor :movements, :qualities, :duration, :weights
 
   def initialize(attrs)
     self.movements = attrs[:moves]
+    self.weights = {}
     set_movement_probabilities
     self.qualities = attrs[:qualities]
     self.duration = attrs[:duration] || 4
-    self.scale = {}
     make_dance
   end
 
@@ -32,30 +34,28 @@ class DanceMaker
     puts
   end
 
-  def set_movement_probabilities
-    total = 0
-    weights = {}
-    movements.each do |move|
-      weight = move[:weight]
-      total += weight
-      weights[total.to_s] = move[:name]
-    end
-  end
-
   def make_moves
     moves = []
     8.times { moves << movement + times_repeated + movement_quality }
     moves
   end
 
-  def movement
-    # TODO: make it so that movements aren't just randomly chosen.
-    # Make it possible to specify the probablity that a move gets selected.
-    num = (1..100).to_a.sample
-    scale.each do |move|
-      if num <= move.key.to_i
+  def set_movement_probabilities
+    total = 0
+    movements.each do |move|
+      weight = move[:weight]
+      total += weight
+      weights[total.to_s] = move[:name]
     end
-    movements.choose_movement
+
+    raise "Probabilities don't total 100" if total != 100
+  end
+
+  def movement
+    num = (1..100).to_a.sample
+    weight = weights.keys.map(&:to_i).sort.select { |weight| num <= weight }.first
+    move = weights[weight.to_s]
+    move
   end
 
   def times_repeated
@@ -71,14 +71,14 @@ end
 
 funny_dance_attrs = {
   moves: [
-    name: "smile", weight: 15,
-    name: "jump", weight: 15,
-    name: "shake", weight: 15,
-    name: "kick", weight: 15,
-    name: "stick out tongue", weight: 5,
-    name: "turn head", weight: 5,
-    name: "give attitude", weight: 12,
-    name: "gesture", weight: 18
+    { name: "smile", weight: 15 },
+    { name: "jump", weight: 15 },
+    { name: "shake", weight: 15 },
+    { name: "kick", weight: 15 },
+    { name: "stick out tongue", weight: 5 },
+    { name: "turn head", weight: 5 },
+    { name: "give attitude", weight: 12 },
+    { name: "gesture", weight: 18 }
   ],
 
   qualities: [
@@ -94,9 +94,10 @@ funny_dance_attrs = {
 
 beautiful_dance_attrs = {
   moves: [
-    "arch",
-    "developpe",
-    "pirouette"
+    { name: "arch", weight: 20 },
+    { name: "lunge", weight: 20 },
+    { name: "developpe", weight: 40 },
+    { name: "pirouette", weight: 20 }
   ],
 
   qualities: [
@@ -109,7 +110,7 @@ beautiful_dance_attrs = {
 }
 
 
-# funny_dance = DanceMaker.new(funny_dance_attrs)
-beautiful_dance = DanceMaker.new(beautiful_dance_attrs)
+funny_dance = DanceMaker.new(funny_dance_attrs)
+# beautiful_dance = DanceMaker.new(beautiful_dance_attrs)
 
 
